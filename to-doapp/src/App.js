2f551e1d1e749126714c04c20e244408e7605c9f
ZoomOutt <stina.kalnina@gmail.com>
2019-10-10 23:58:20 +0300

    Making To-Do App

--- to-doapp/src/App.js
+++ to-doapp/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ todos, setTodos ] = useState([
+		{
+			text: 'Learn about React',
+			isColpeted: false
+		},
+		{
+			text: 'Meet friend for luch',
+			isColpeted: false
+		},
+		{
+			text: 'Build really cool todo app',
+			isColpeted: false
+		}
+	]);
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
